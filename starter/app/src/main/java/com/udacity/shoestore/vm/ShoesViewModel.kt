4515c67b5dd014bf9ef098baf3f190546ab23dd7
miro <minev.miro@gmail.com>
2020-12-26 21:23:03 +0200

    create show list view model

---
+++ starter/app/src/main/java/com/udacity/shoestore/vm/ShoesViewModel.kt
@@ -0,0 +1,12 @@
+package com.udacity.shoestore.vm
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
+
+class ShoesViewModel : ViewModel() {
+    private val _shoeList = MutableLiveData<List<Shoe>>()
+    val shoeList: LiveData<List<Shoe>>
+        get() = _shoeList
+}
