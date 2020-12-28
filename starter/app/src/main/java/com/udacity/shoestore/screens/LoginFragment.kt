7b867518fc8609db0c3678afd0b5372e7b7c029b
miro <minev.miro@gmail.com>
2020-12-29 01:16:07 +0200

    login input validation

--- starter/app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -11,20 +12,51 @@ import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_login, container, false)
-        binding.buttonSignUp.setOnClickListener { view ->
-            view.findNavController()
-                .navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(binding.inputEmail.text.toString()))
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        binding.inputEmail.doAfterTextChanged {
+            if (!it.isNullOrBlank()) {
+                binding.inputEmailLayout.error = null
+            }
         }
-        binding.buttonLogin.setOnClickListener { view ->
-            view.findNavController()
-                .navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(binding.inputEmail.text.toString()))
+
+        binding.inputPassword.doAfterTextChanged {
+            if (!it.isNullOrBlank()) {
+                binding.inputPasswordLayout.error = null
+            }
         }
+
+        binding.buttonLogin.setOnClickListener(::next)
+
+        binding.buttonSignUp.setOnClickListener(::next)
+
         return binding.root
     }
 
-    // TODO: save email and password state, maybe use a "remember me" checkbox
+    private fun next(view: View) {
+        if (validateInputs()) {
+            view.findNavController()
+                .navigate(LoginFragmentDirections
+                    .actionLoginFragmentToWelcomeFragment(binding.inputEmail.text.toString()))
+        }
+    }
+
+    private fun validateInputs(): Boolean {
+        var valid = true
+        if (binding.inputEmail.text.isNullOrEmpty()) {
+            binding.inputEmailLayout.error = getString(R.string.login_email_error)
+            valid = false
+        }
+
+        if (binding.inputPassword.text.isNullOrEmpty()) {
+            binding.inputPasswordLayout.error = getString(R.string.login_password_error)
+            valid = false
+        }
+
+        return valid
+    }
 }
